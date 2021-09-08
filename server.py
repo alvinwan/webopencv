93c7f0be76b52f46bf9fa3c78124921d8f2e43c4
Alvin Wan <alvinwan1@gmail.com>
2021-09-07 23:31:56 -0700

    reorg static files into static/ dir

--- server.py
+++ server.py
@@ -89,12 +89,12 @@ class VideoTransformTrack(MediaStreamTrack):
 
 
 async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    content = open(os.path.join(ROOT, "static", "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
 
 
 async def javascript(request):
-    content = open(os.path.join(ROOT, "client.js"), "r").read()
+    content = open(os.path.join(ROOT, "static", "client.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
 
 

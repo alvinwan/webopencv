2cec44a4379a9b2e370c0d5a11a2071a3789ee08
Alvin Wan <alvinwan1@gmail.com>
2021-09-14 00:51:42 -0700

    move examples to own file

---
+++ wcv2/examples.py
@@ -0,0 +1,37 @@
+import cv2
+
+
+def transform_cartoon(img, frame=None):
+    # prepare color
+    img_color = cv2.pyrDown(cv2.pyrDown(img))
+    for _ in range(6):
+        img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+    img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+    # prepare edges
+    img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img_edges = cv2.adaptiveThreshold(
+        cv2.medianBlur(img_edges, 7),
+        255,
+        cv2.ADAPTIVE_THRESH_MEAN_C,
+        cv2.THRESH_BINARY,
+        9,
+        2,
+    )
+    img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+    # combine color and edges
+    img = cv2.bitwise_and(img_color, img_edges)
+    return img
+
+
+def transform_edge_detection(img, frame=None):
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+    return img
+
+
+def transform_rotate(img, frame=None):
+    rows, cols, _ = img.shape
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+    img = cv2.warpAffine(img, M, (cols, rows))
+    return img

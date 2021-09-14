5a81fae0091d9c7b1e013c89a01cc8ca53fccc36
Alvin Wan <alvinwan1@gmail.com>
2021-09-14 01:38:10 -0700

    using web-framework-agnostic func

--- wcv2/aio.py
+++ wcv2/aio.py
@@ -3,7 +3,9 @@
 from aiohttp import web
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
 from av import VideoFrame
+from .app import on_offer, on_shutdown
 from . import transforms
+from .utils import ROOT, logger
 
 import asyncio
 import json
@@ -12,103 +14,15 @@ import os
 import uuid
 
 
-ROOT = os.path.dirname(__file__)
-logger = logging.getLogger("pc")
-pcs = set()
-
-
-class VideoTransformTrack(MediaStreamTrack):
-    """
-    A video stream track that transforms frames from an another track.
-    """
-
-    kind = "video"
-
-    def __init__(self, track, transform):
-        super().__init__()  # don't forget this!
-        self.track = track
-        self.transform = transform
-
-    async def recv(self):
-        frame = await self.track.recv()
-        
-        if self.transform in transforms.get_transform_names():
-            # convert from video frame to numpy array
-            img = frame.to_ndarray(format="bgr24")
-
-            # run image transformation
-            transform = transforms.get_transform(self.transform)
-            img = transform(img, frame)
-
-            # Write back to video frame, with timestamp
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-
-        return frame
-
-
 async def offer(request):
     params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-
-    pc = RTCPeerConnection()
-    pc_id = "PeerConnection(%s)" % uuid.uuid4()
-    pcs.add(pc)
-
-    def log_info(msg, *args):
-        logger.info(pc_id + " " + msg, *args)
-
-    log_info("Created for %s", request.remote)
-
-    @pc.on("datachannel")
-    def on_datachannel(channel):
-        @channel.on("message")
-        def on_message(message):
-            if isinstance(message, str) and message.startswith("ping"):
-                channel.send("pong" + message[4:])
-
-    @pc.on("iceconnectionstatechange")
-    async def on_iceconnectionstatechange():
-        log_info("ICE connection state is %s", pc.iceConnectionState)
-        if pc.iceConnectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
-
-    @pc.on("track")
-    def on_track(track):
-        log_info("Track %s received", track.kind)
-
-        local_video = VideoTransformTrack(
-            track, transform=params["video_transform"]
-        )
-        pc.addTrack(local_video)
-
-        @track.on("ended")
-        async def on_ended():
-            log_info("Track %s ended", track.kind)
-
-    # handle offer
-    await pc.setRemoteDescription(offer)
-
-    # send answer
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-
+    pc = await on_offer(params, sender=request.remote)
     return web.Response(
         content_type="application/json",
         text=json.dumps(
             {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
         ),
     )
-
-
-async def on_shutdown(app):
-    # close peer connections
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
 
 
 def static(path, **kwargs):
